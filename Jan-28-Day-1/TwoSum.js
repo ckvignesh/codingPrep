44c7b87ea3228be261241f85d839c53365e003f1
ckvignesh <vigneshckprakash@gmail.com>
2022-01-28 23:00:14 +0530

    day 1

---
+++ Jan-28-Day-1/TwoSum.js
@@ -0,0 +1,9 @@
+var twoSum = function (nums, target) {
+  var hash = {};
+  var len = nums.length;
+  for (var i = 0; i < len; i++) {
+    if (nums[i] in hash) return [hash[nums[i]], i];
+    hash[target - nums[i]] = i;
+  }
+  return [-1, -1];
+};
