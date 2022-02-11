49a2a73a4da05fe5ee6c45f185b6ff81c26ed826
ckvignesh <vigneshckprakash@gmail.com>
2022-02-12 00:00:00 +0530

    feb 11

--- Feb-11-Day-15/staircase.js
+++ Feb-11-Day-15/staircase.js
@@ -1,6 +1,7 @@
 function staircase(n) {
-    let char = "#"
-    let space = " "
-    for(let i=1; i<=n; i++){
-        console.log(space.repeat(n-i)+char.repeat(i))
-    }
+  let char = "#";
+  let space = " ";
+  for (let i = 1; i <= n; i++) {
+    console.log(space.repeat(n - i) + char.repeat(i));
+  }
+}
