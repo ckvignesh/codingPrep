8b5385e2b4c7de00b0d27fbbea451def02314822
ckvignesh <vigneshckprakash@gmail.com>
2022-02-11 13:29:16 +0530

    interim update

--- Feb-10-Day-14/Celsius Fahrenheit equality finder.js
+++ Feb-10-Day-14/Celsius Fahrenheit equality finder.js
@@ -8,9 +8,27 @@ function convertToC(fahrenheit) {
   return celsius;
 }
 
-for (let i = 500; i > -500; i--) {
-  if (convertToC(i) === i) {
-    console.log(i);
+// for (let i = 500; i > -500; i--) {
+//   if (convertToC(i) === i) {
+//     console.log(i);
+//   }
+// }
+
+let low = -500;
+let high = 500;
+let item = 40;
+
+while (low <= high) {
+  const mid = Math.floor((low + high) / 2);
+  const guess = 0;
+
+  if (guess === convertToC(guess)) {
+    return guess;
+  }
+  if (guess > item) {
+    high = mid - 1;
+  } else {
+    low = mid + 1;
   }
 }
 
