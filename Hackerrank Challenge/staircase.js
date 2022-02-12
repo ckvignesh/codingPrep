85d855e1acc05522e4f04212c08c1c5f02d30027
ckvignesh <vigneshckprakash@gmail.com>
2022-02-12 20:05:29 +0530

    feb 12

--- Hackerrank Challenge/staircase.js
+++ Hackerrank Challenge/staircase.js
@@ -4,4 +4,9 @@ function staircase(n) {
   for (let i = 1; i <= n; i++) {
     console.log(space.repeat(n - i) + char.repeat(i));
   }
+  return n + "steps";
 }
+
+console.log(staircase(10));
+console.log(staircase(18));
+console.log(staircase(1));
