f5a9fb8018da5e80220bd827387011c17372353a
ckvignesh <vigneshckprakash@gmail.com>
2022-10-09 21:45:11 +0530

    ts prep

--- angular-learning/datatypes.ts
+++ angular-learning/datatypes.ts
@@ -1,3 +1,35 @@
-let lName = "john";
+// let lName: string = "john";
 
-console.log(lName);
+// console.log(lName);
+
+// let newName = lName.toUpperCase();
+// console.log(newName);
+
+// let dob = "22";
+// let result = parseInt(dob);
+// console.log(result);
+
+// let isVal:boolean = false;
+// console.log(isVal);
+
+// let empList =["test", "vignesh", 123];
+// console.log(empList);
+
+// let empList: Array<string>;
+
+// empList = ["terr", 123]
+let list: Array<number>;
+
+list = [1,2,3,4,5,6,7,8,9,0]
+
+// let results = list.filter((unit)=>unit>=3);
+
+// let results = list.find((n)=>n===11)
+// if (results == undefined|| null){
+//     console.log("no number found");
+// } else {
+//     console.log(results);
+// }
+
+// let sum = list.reduce((acc, num)=>acc + num);
+// console.log(sum);
